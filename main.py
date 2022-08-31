de7b5601323411014f28f9b9c6260c09da113042
sujemuje <mateusz.sujewicz@op.pl>
2022-09-01 00:09:37 +0200

    chuj nie dziala

--- main.py
+++ main.py
@@ -7,6 +7,24 @@ client = discord.Client(intents=discord.Intents.all())
 tree = discord.app_commands.CommandTree(client=client)
 
 
+hatsune_group = discord.app_commands.Group(
+    name='hatsune_database',
+    description='idk',
+    guild_ids=[602533696272203815]
+)
+
+
+@hatsune_group.command(
+    name='hatsune_miku',
+    description='lol'
+)
+async def __command_hatsune_miku(interaction: discord.Interaction) -> None:
+    await interaction.response.send_message('hatsune miku')
+
+
+tree.add_command(hatsune_group)
+
+
 @tree.command(
     name='add_command',
     description='Add your own command'
@@ -26,7 +44,7 @@ async def __command_add_command(
         await _interaction.response.send_message(
             'Congratulations, you successfully created this command!'
         )
-    
+
     await tree.sync()
     await \
         interaction.edit_original_response(content='Command successfully created')
